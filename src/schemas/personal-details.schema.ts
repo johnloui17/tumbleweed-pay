cf0ac2030fad479cfe3c9950ab2eb9fda1d8e08e
johnloui17 <jonloui17@gmail.com>
2026-05-23 03:43:51 +0530

    feat: reposition tooltip to bottom and add regex name validation to personal details schema

--- src/schemas/personal-details.schema.ts
+++ src/schemas/personal-details.schema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod'
 
 export const nameSchema = z.object({
-  firstName: z.string().min(2, 'First name must be at least 2 characters'),
-  lastName: z.string().min(2, 'Last name must be at least 2 characters'),
+  firstName: z.string().min(2, 'First name must be at least 2 characters').regex(/^[A-Za-z\s]+$/, 'Your name has numbers ?'),
+  lastName: z.string().min(2, 'Last name must be at least 2 characters').regex(/^[A-Za-z\s]+$/, 'Your name has numbers ?'),
 })
 
 export type NameFormData = z.infer<typeof nameSchema>
